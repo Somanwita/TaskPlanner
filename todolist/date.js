399933ed9019bfea86bcdb8af7162ab2c2c7dbe8
Somanwita <somanwita21@gmail.com>
2020-08-08 13:34:31 -0400

    ToDoListFinalCommit

---
+++ todolist/date.js
@@ -0,0 +1,25 @@
+
+exports.getDate =  function() {
+  const today = new Date();
+  //let currentDay = today.getDay();
+  const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  return today.toLocaleDateString("en-US", options);
+}
+
+module.exports.getDay = getDay;
+
+function getDay() {
+  const today = new Date();
+//  let currentDay = today.getDay();
+  const options = {
+    weekday: "long",
+  };
+
+  return today.toLocaleDateString("en-US", options);
+
+}
